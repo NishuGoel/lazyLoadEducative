e8c72dda2a4865bc82be4a34860c134ba480460e
nishugoe <nishugoe@in.ibm.com>
2020-04-02 02:31:44 +0530

    lazily hello

---
+++ app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+// import { HelloComponent } from './hello.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MainPageComponent } from './main-page/main-page.component';
+
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
+  declarations: [ AppComponent, MainPageComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
---
+++ hello/hello.module.ts
@@ -0,0 +1,10 @@
+  
+import { NgModule } from '@angular/core';
+import { HelloComponent } from './hello.component';
+import { HelloRoutingModule } from './hello-routing.module';
+
+@NgModule({
+  imports:  [HelloRoutingModule ],
+  declarations: [HelloComponent],
+})
+export class HelloModule { }
